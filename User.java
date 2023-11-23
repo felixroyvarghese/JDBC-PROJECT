2b6922c37f36efa526e33b96692e8b998417a60b
felixroyvarghese <148309813+felixroyvarghese@users.noreply.github.com>
2023-11-23 17:37:16 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,36 @@
+package com.training.pojo;
+
+public class User {
+	
+	private String user;
+	private String emailid;
+	private int supercoins;
+	private String password;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public int getSupercoins() {
+		return supercoins;
+	}
+	public void setSupercoins(int supercoins) {
+		this.supercoins = supercoins;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+
+}
